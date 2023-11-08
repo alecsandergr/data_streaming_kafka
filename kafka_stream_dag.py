c2166de039af9cc0620dc999e89b79a94bdc29a5
Alecsander Guimaraes <leelecko@gmail.com>
2023-11-08 15:59:13 -0300

    adding the api extractor

--- kafka_stream_dag.py
+++ kafka_stream_dag.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+
 from kafka_streaming_service import initiate_stream
 
 # Configuration for the DAG's start date
